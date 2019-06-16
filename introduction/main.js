e537685f52b1469fd50458acfd2c50c635bf9ce8
jeff0880509 <50113444+jeff0880509@users.noreply.github.com>
2019-06-16 10:46:34 +0800

    Update main.js

--- introduction/main.js
+++ introduction/main.js
@@ -6,12 +6,12 @@ $(document).ready(function()
         var ran =Math.floor(Math.random()*num);
         $("#ccc").text($("#choices li").eq(ran).text());
 		if(ran==0)
-			$("img").attr("src","RandomSelector/volleyball01.jpg");
+			$("img#bbb").attr("src","RandomSelector/volleyball01.jpg");
 		else if(ran==1)
-			$("img").attr("src","RandomSelector/volleyball02.jpg");
+			$("img#bbb").attr("src","RandomSelector/volleyball02.jpg");
 		else if(ran==2)
-			$("img").attr("src","RandomSelector/volleyball03.jpg");
+			$("img#bbb").attr("src","RandomSelector/volleyball03.jpg");
 		else if(ran==3)
-			$("img").attr("src","RandomSelector/volleyball04.jpg");
+			$("img#bbb").attr("src","RandomSelector/volleyball04.jpg");
     }); 
 });
